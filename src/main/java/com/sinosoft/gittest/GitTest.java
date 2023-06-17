32fd4086c101eb54a28b1d6b3477391ac79d9ed5
draha_lalala <827305418@qq.com>
2023-06-17 17:49:35 +0800

    idea第一次git提交

---
+++ src/main/java/com/sinosoft/gittest/GitTest.java
@@ -0,0 +1,13 @@
+package com.sinosoft.gittest;
+
+/**
+ * @author  : zhangbin
+ * create : 2023/6/17 17:26d
+ * description :
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+        System.out.println("你好！");
+    }
+}
