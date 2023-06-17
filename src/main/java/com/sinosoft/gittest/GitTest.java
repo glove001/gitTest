fcb42f3fcab8f7011fd19fb5b60859ee498dfdf5
draha_lalala <827305418@qq.com>
2023-06-17 21:35:24 +0800

    idea第三次git提交

--- src/main/java/com/sinosoft/gittest/GitTest.java
+++ src/main/java/com/sinosoft/gittest/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("Hello,Git!");
         System.out.println("你好！");
         System.out.println("lalal");
+        System.out.println("yayayaya");
     }
 }
